869e400afdf824f5d69d73522be341baae1d985a
osama askar <osamaaskar0100@gmail.com>
2018-12-03 13:51:33 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    "use strict"
+
+    // Make Background Size As Same As Window Size
+    var winh = $(window).height();
+    var navh = $(".navbar").innerHeight();
+
+    $(".header").height(winh);
+
+    
+});
